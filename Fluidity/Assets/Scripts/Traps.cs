c55cfd3cc0bf0065c96ce739fb7166738c5817d1
Leifeon001 <tristen.howk@yahoo.com>
2020-07-18 17:52:25 -0400

    Trap Update
    Liquid Traps updated that if player stays to long changes to gas

--- Fluidity/Assets/Scripts/Traps.cs
+++ Fluidity/Assets/Scripts/Traps.cs
@@ -11,7 +11,10 @@ public class Traps : MonoBehaviour
         Gas
     }
     public State Matter;
-
+    //Timer for the Liquid State Trap. If Timer reaches 0 player turns to gas state
+    [Header("Timer")]
+    public float Timer = 3.0f;
+    public bool TimerAct;
     public void OnTriggerEnter2D(Collider2D other)
     {
         Player PisHere = other.GetComponent<Player>();
@@ -19,6 +22,35 @@ public class Traps : MonoBehaviour
         {
             string temp = Matter.ToString();
             PisHere.ChangeState(temp);
+        }
+    }
+
+    public void OnTriggerStay2D(Collider2D other)
+    {
+        if(Matter.ToString() == "Liquid" && other.name == "Player")
+        {
+            Debug.Log("Player is Staying");
+            TimerAct = true;
+        }
+    }
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.name == "Player")
+        {
+            Timer = 3.0f;
+            TimerAct = false;
+        }
+    }
+    public void Update()
+    {
+        if(TimerAct == true)
+        {
+            Timer -= Time.deltaTime;
+            if(Timer <= 0)
+            {
+                Player Temp = FindObjectOfType<Player>();
+                Temp.ChangeState("Gas");
+            }
         }
     }
 }
