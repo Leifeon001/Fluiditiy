ed44fc5efb71f8e58427e3eca4556be2b3d270e1
YoungSensei <59657629+YoungSensei@users.noreply.github.com>
2020-07-21 01:22:08 -0400

    updates

--- Fluidity/Assets/StateSwitchInput.cs
+++ Fluidity/Assets/StateSwitchInput.cs
@@ -6,33 +6,45 @@ public class StateSwitchInput : MonoBehaviour
 {
     Rigidbody2D Playerrigidbody2D;
     Player ply;
-    int state = 1;
+    public int state = 1;
     Renderer rend;
+    BoxCollider2D box;
+    public AudioSource Audi;
+    public AudioClip clip1, clip2, clip3;
     // Start is called before the first frame update
     void Start()
     {
+        Audi = GetComponent<AudioSource>();
+        box = GetComponent<BoxCollider2D>();
         Playerrigidbody2D = GetComponent<Rigidbody2D>();
         ply = GetComponent<Player>();
         rend = GetComponent<Renderer>();
+
+        Change();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Change();
+        
 
-        if (Input.GetKeyDown(KeyCode.Q))
+        if (Input.GetKeyDown(KeyCode.Q)&& !Audi.isPlaying)
         {
             state--;
+           
             if (state < 1)
                 state = 3;
+            Change();
         }
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E)&& !Audi.isPlaying)
         {
             state++;
+            
             if (state > 3)
                 state = 1;
+            Change();
         }
+
 
 
 
@@ -46,25 +58,38 @@ public class StateSwitchInput : MonoBehaviour
             case 1:
                 //Ice
                 rend.material.SetColor("_Color", Color.red);
+                if (!Audi.isPlaying)
+                    Audi.PlayOneShot(clip1);
                 Playerrigidbody2D.gravityScale = 3;
                 ply.speedX = 4f;
                 ply.xForce = 17;
+                box.size = new Vector2(1, 1);
+                box.offset = new Vector2(0, 0);
                 break;
 
             case 2:
                 //Gas
+                if (!Audi.isPlaying)
+                    Audi.PlayOneShot(clip2);
                 rend.material.SetColor("_Color", Color.gray);
                 Playerrigidbody2D.gravityScale = -1;
                 ply.speedX = 2f;
                 ply.xForce = 14;
+                box.size = new Vector2(1, 1);
+                box.offset = new Vector2(0, 0);
                 break;
 
             case 3:
                 //Water
+                if (!Audi.isPlaying)
+                    Audi.PlayOneShot(clip3);
                 rend.material.SetColor("_Color", Color.blue);
                 Playerrigidbody2D.gravityScale = 1;
                 ply.speedX = 2f;
                 ply.xForce = 8;
+                box.size = new Vector2(1, .2f);
+                box.offset = new Vector2(0, .4f);
+                
                 break;
         }
     }
