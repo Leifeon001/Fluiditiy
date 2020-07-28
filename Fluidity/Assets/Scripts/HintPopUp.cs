9a1914ad9ab29cda72ae29a13ed375ab5c46d03d
Raj Shah <rvshah269@gmail.com>
2020-07-28 00:31:58 -0400

    UI cleaning

---
+++ Fluidity/Assets/Scripts/HintPopUp.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HintPopUp : MonoBehaviour
+{
+    public GameObject Hint1;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collision detected");
+        Hint1.SetActive(true);
+    }
+}
